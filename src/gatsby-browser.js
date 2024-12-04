af47d13a95d3e0934c7fd6e68b2cb22b09d43aae
kiepon <dmitrii.domnin313@gmail.com>
2024-12-04 19:21:52 +0300

    Updates

--- src/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { motion } from "framer-motion";
-
-export const wrapRootElement = ({ element }) => (
-  <motion.div initial="hidden" animate="visible" exit="hidden">
-    {element}
-  </motion.div>
-);
